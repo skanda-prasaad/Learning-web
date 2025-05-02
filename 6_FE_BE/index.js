a3d14f7022700fd0d7d8d56a8fbae884d2160e6a
Skanda Prasad <skandaprasad595@gmail.com>
2025-05-02 13:47:03 +0530

    commit-2

--- 6_FE_BE/index.js
+++ 6_FE_BE/index.js
@@ -7,22 +7,26 @@ const users = [];
 app.use(express.json());
 
 function auth(req, res, next) {
-  const token = req.headers.authorization;
-  if(!token){
-    res.json({
-      message: "You are not logged in",
-    });
+  const token = req.headers.token;
+  if (!token) {
+    return res.status(401).json({ message: "You are not logged in" });
   }
-  const decodedinfo = jwt.verify(token, JWT_SECRET);
-  if (decodedinfo.username) {
-    req.user = decodedinfo;
-    next();
-  } else {
-    res.json({
-      message: "You are not logged in",
-    });
+  try {
+    const decodedinfo = jwt.verify(token, JWT_SECRET);
+    if (decodedinfo.username) {
+      req.user = decodedinfo;
+      next();
+    } else {
+      res.status(401).json({ message: "You are not logged in" });
+    }
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
   }
 }
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/Public/index.html");
+});
 
 app.post("/signup", function (req, res) {
   const { username, password } = req.body;
@@ -41,33 +45,26 @@ app.post("/signup", function (req, res) {
 });
 
 app.post("/signin", function (req, res) {
-  // req.user
   const { username, password } = req.body;
   if (!username || !password) {
     return res.status(400).send("Username and password are required...");
   }
   const user = users.find(
-    (user) => user.username == username && user.password == password
+    (user) => user.username === username && user.password === password
   );
   if (user) {
-    const token = jwt.sign(
-      {
-        username: username,
-      },
-      JWT_SECRET
-    );
-    res.json({
+    const token = jwt.sign({ username: username }, JWT_SECRET);
+    return res.json({
       username: username,
       token: token,
     });
   } else {
-    return res.status(400).send("Invalid credentials..");
+    return res.status(400).send("Invalid credentials.");
   }
-  res.status(200).send("User signin successufull..");
 });
 
-app.get("/me", auth , function (req, res) {
-  const username = req.user.usernmae;
+app.get("/me", auth, function (req, res) {
+  const username = req.user.username;
   const user = users.find((user) => user.username == username);
   if (user) {
     res.json({
