6e36ba660666ea4195e3778c73ac38743e3b90c2
Skanda Prasad <skandaprasad595@gmail.com>
2025-05-09 22:56:48 +0530

    zod

--- 7_Mongo/server.js
+++ 7_Mongo/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const { TodoModel, UserModel } = require("./db");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const { z } = require("zod");
 
 const app = express();
 mongoose.connect(
@@ -12,22 +13,59 @@ app.use(express.json());
 
 const JWT_SECRET = "secretKey";
 
+// Zod schemas
+const signupSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+  name: z.string().min(1),
+});
+
+const signinSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+const todoSchema = z.object({
+  title: z.string().min(1),
+  done: z.boolean(),
+});
+
+// Signup Route
 app.post("/signup", async function (req, res) {
-  const { email, password, name } = req.body;
+  const parseResult = signupSchema.safeParse(req.body);
+  if (!parseResult.success) {
+    return res
+      .status(400)
+      .json({ message: "Invalid input", errors: parseResult.error.errors });
+  }
+
+  const { email, password, name } = parseResult.data;
   const hashPasw = await bcrypt.hash(password, 5);
   try {
-    await UserModel.create({ email, hashPasw0, name });
+    await UserModel.create({ email, password: hashPasw, name });
     res.json({ message: "You are signed up!" });
   } catch (error) {
     res.status(400).json({ message: "User already exists!" });
   }
 });
 
+// Signin Route
 app.post("/signin", async function (req, res) {
-  const { email, password } = req.body;
-  const user = await UserModel.findOne({ email, password });
+  const parseResult = signinSchema.safeParse(req.body);
+  if (!parseResult.success) {
+    return res
+      .status(400)
+      .json({ message: "Invalid input", errors: parseResult.error.errors });
+  }
 
-  if (user) {
+  const { email, password } = parseResult.data;
+  const user = await UserModel.findOne({ email });
+  if (!user) {
+    return res.status(403).json({ message: "User not found!" });
+  }
+
+  const passMatch = await bcrypt.compare(password, user.password);
+  if (passMatch) {
     const token = jwt.sign({ id: user._id.toString() }, JWT_SECRET);
     res.json({ token, message: "You are signed in!" });
   } else {
@@ -35,6 +73,7 @@ app.post("/signin", async function (req, res) {
   }
 });
 
+// Auth Middleware
 function auth(req, res, next) {
   const token = req.headers.authorization;
   try {
@@ -46,15 +85,26 @@ function auth(req, res, next) {
   }
 }
 
+// Create Todo
 app.post("/todo", auth, async function (req, res) {
-  const { title, done } = req.body;
+  const parseResult = todoSchema.safeParse(req.body);
+  if (!parseResult.success) {
+    return res
+      .status(400)
+      .json({ message: "Invalid todo data", errors: parseResult.error.errors });
+  }
+
+  const { title, done } = parseResult.data;
   await TodoModel.create({ title, done, userId: req.userId });
   res.json({ message: "Todo created" });
 });
 
+// Get Todos
 app.get("/todo", auth, async function (req, res) {
   const todos = await TodoModel.find({ userId: req.userId });
   res.json({ todos });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
