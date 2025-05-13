07633eac410394beca79c423beaf8a9ee4fe7489
Skanda Prasad <skandaprasad595@gmail.com>
2025-05-13 18:54:43 +0530

    Update App.jsx

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,13 +1,42 @@
 import { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  function OnClickHandler() {
-    setCount(count + 1);
-  }
+const Display = ({ title, description, done }) => {
   return (
     <div>
-      <button onClick={OnClickHandler}>Counter {count}</button>
+      <h1>{title}</h1>
+      <h2>{description}</h2>
+      <h3>{done ? "Completed" : "Not Completed"}</h3>
+    </div>
+  );
+};
+
+const App = () => {
+  const [todo, setTodo] = useState([
+    {
+      title: "React",
+      description: "Library",
+      read: false,
+    },
+  ]);
+
+  function addTodo() {
+    setTodo([
+      ...todo,
+      { title: "New Task", description: "Added item", read: false },
+    ]);
+  }
+
+  return (
+    <div>
+      <button onClick={addTodo}>Add todo</button>
+      {todo.map((item, index) => (
+        <Display
+          key={index}
+          title={item.title}
+          description={item.description}
+          done={item.read}
+        />
+      ))}
     </div>
   );
 };
