24ab8b1dcb0581cbb60caa357481dee15f64badd
Skanda Prasad <skandaprasad595@gmail.com>
2025-06-01 19:22:37 +0530

    coomit

---
+++ React/vite-project/components/Create.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function Create({ setUsers }) {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(18);
+
+  function Handler(e) {
+    e.preventDefault();
+
+    setUsers((prevUsers) => [
+      ...prevUsers,
+      {
+        Name: name,
+        Age: Number(age), // ensures age is stored as a number
+      },
+    ]);
+
+    setName("");
+    setAge(18); // reset age to default value instead of ""
+  }
+  return (
+    <div>
+      <h1>Form Registration</h1>
+      <form onSubmit={Handler}>
+        <input
+          type="text"
+          placeholder="Fullname"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="number"
+          value={age}
+          placeholder="Age"
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,44 +1,14 @@
 import { useState } from "react";
-
-const Display = ({ title, description, done }) => {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <h2>{description}</h2>
-      <h3>{done ? "Completed" : "Not Completed"}</h3>
-    </div>
-  );
-};
-
-const App = () => {
-  const [todo, setTodo] = useState([
-    {
-      title: "React",
-      description: "Library",
-      read: false,
-    },
+import Create from "../components/create";
+import Read from "../components/read";
+export default function App() {
+  const [users, setUsers] = useState([
+    {Name : "John" , Age : 20}
   ]);
-
-  function addTodo() {
-    setTodo([
-      ...todo,
-      { title: "New Task", description: "Added item", read: false },
-    ]);
-  }
-
   return (
     <div>
-      <button onClick={addTodo}>Add todo</button>
-      {todo.map((item, index) => (
-        <Display
-          key={index}
-          title={item.title}
-          description={item.description}
-          done={item.read}
-        />
-      ))}
+      <Create setUsers={setUsers} />
+      <Read users={users} />
     </div>
   );
-};
-
-export default App;
+}
